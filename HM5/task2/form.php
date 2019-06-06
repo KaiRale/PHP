9c1727ca6bef3740f64c6d4a34093b8c0cbba7ff
Анастасия <48827161+KaiRale@users.noreply.github.com>
2019-06-06 16:36:00 +0300

    Update form.php

--- HM5/task2/form.php
+++ HM5/task2/form.php
@@ -10,15 +10,17 @@
 </head>
 <body>
 
-
+<div class="divForm">
+<img src="qFQfSA7v_Q.png">
 <form class="someForm" name="someForm" method="post" action="HM.php">
+
     <h1 title="Сокращатель"> Введите вашу ссылку</h1>
     <p class="errors" id="errors"></p>
     <p><input class="url" type="url" name="for_url" placeholder="url"></p>
     <p><input class="forSubm" type="submit" value="Сократить"> </p>
     <p class="withoutError" id="forUrl"></p>
 </form>
-
+</div>
 <script src="ajax.js"></script>
 </body>
 </html>
